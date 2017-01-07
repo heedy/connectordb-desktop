29ce58255d3b5ad38281faed12d28f447e1f7670
Daniel R. Kumor <daniel@dkumor.com>
2017-01-07 05:12:13 -0500

    Initial release setup

---
+++ src/windowsapp.py
@@ -0,0 +1,13 @@
+# In order to create an executable, the windows pyinstaller needs the libraries that will be used
+#in all the windows plugins. We import them here directly
+
+import pyHook, pythoncom
+import win32gui
+from multiprocessing import Process, Value, freeze_support
+import csv #Not sure why... but pyinstaller complains
+
+import laptoplogger
+
+if __name__=="__main__":
+    freeze_support()    #Need to have freeze support
+    laptoplogger.runapp()
